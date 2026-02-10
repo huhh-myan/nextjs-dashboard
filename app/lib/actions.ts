27dff9f80c149458c21f36b4cf789db01e9a84fc
huhh-myan <swaroop073@gmail.com>
2026-02-10 22:46:11 +0530

    added auth credentials provider

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,8 +3,33 @@ import { z } from 'zod';
 import postgres from 'postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+import { error } from 'console';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+
+
+export async function authenticate(
+    prevState:string | undefined,
+    formData: FormData,
+) {
+    try{
+        await signIn('credentials', formData);
+    }catch(error){
+        if(error instanceof AuthError){
+            switch(error.type){
+                case "CredentialsSignin":
+                    return "Invalid Credentials.";
+                default:
+                    return "Something went wrong.";
+            }
+        }
+        throw error;
+    }
+}
+
 
 
 export type State = {
